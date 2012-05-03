183ad58ccf1f90f0f75228ee84b009e87995e0f5
CindeeM <cindee@berkeley.edu>
2012-05-03 08:18:45 -0700

    added setup code for longitudinal processing and qa tools

---
+++ pib/pib_realign_coreg.py
@@ -0,0 +1,83 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+#!/usr/bin/env python
+
+import sys, os
+import wx
+from glob import glob
+sys.path.insert(0, '/home/jagust/cindeem/CODE/PetProcessing')
+import preprocessing as pp
+import base_gui as bg
+import rapid_art as qa
+import logging, logging.config
+from time import asctime
+
+if __name__ == '__main__':
+    """
+    Uses specified PIB directory
+    finds pib nifti files
+    check number of files
+    realigned 6-end to frame 17
+    sum 1-5
+    coreg summed 1-5 to mean from realign, and apply to each frame
+    run qa on scans
+    """
+    # start wx gui app
+    app = wx.App()
+
+    arda = '/home/jagust/arda/lblid'
+    root = bg.SimpleDirDialog(prompt='Choose PIB data dir',
+                              indir = '/home/jagust')
+    
+    cleantime = asctime().replace(' ','-').replace(':', '-')
+    logfile = os.path.join(root,'logs',
+                           'setup_pibdirectory_%s.log'%(cleantime))
+
+    log_settings = pp.get_logging_configdict(logfile)
+    logging.config.dictConfig(log_settings)
+
+    
+    tracer = 'PIB'
+    user = os.environ['USER']
+    logging.info('###START Setup Directory :::')
+    logging.info('###TRACER Setup %s  :::'%(tracer))
+    logging.info('###USER : %s'%(user))
+    subs = bg.MyDirsDialog(prompt='Choose Subjects ',
+                           indir='%s/'%root)
+    for sub in subs:
+        _, subid = os.path.split(sub)
+
+        nifti = glob('%s/pib/%s_PIB*.nii'%(sub, subid))
+        nifti.sort()
+        if len(nifti) < 34:
+            logging.error('%s only has %d frames, RUNBYHAND'%(subid,
+                                                              len(nifti)))
+            continue
+              
+        rlgnout, newnifti = pp.realigntoframe17(nifti)
+        tmpparameterfile = rlgnout.outputs.realignment_parameters
+        realigndir, _ = os.path.split(tmpparameterfile)
+        sum1_5 = pp.make_summed_image(newnifti[:5],
+                                      prefix='sum1_5_')
+        mean_img = rlgnout.outputs.mean_image
+        #coregister 1-5 to mean
+        crg_out = pp.simple_coregister(mean_img, sum1_5, newnifti[:5])
+        if crg_out.runtime.returncode is not 0:
+            logging.error('Failed to coreg 1-5 to mean for  %s' % subid)
+            continue
+        # grab all realigned files
+        allrealigned = crg_out.outputs.coregistered_files + \
+                       rlgnout.outputs.realigned_files
+        # make new mean files(s) based on fully realigned files
+        # 1. first 20 mins for coreg (frames 1-23)
+        # 2. 40-60 mins for possible SUVR (frames 28-31)
+        mean_20min = pp.make_mean_20min(allrealigned)
+        mean_40_60min = pp.make_mean_40_60(allrealigned)
+
+        # QA
+        # make 4d for QA
+        qadir, exists = make_qa_dir(realigndir, name='data_QA')
+        data4d = qa.make_4d_nibabel(allrealigned, outdir=qadir)
+        snrimg = qa.gen_sig2noise_img(data4d,qadir)
+        artout = run_artdetect(data4d,tmpparameterfile)
+        screen_data_dirnme(data4d, qadir)
---
+++ qa.py
@@ -0,0 +1,189 @@
+import os
+import numpy as np
+import nibabel as ni
+import nipype.algorithms.rapidart as rapidart
+from glob import glob
+import nipype.interfaces.fsl as fsl
+import nipype.algorithms.misc as misc
+import json
+import nipy
+import nipy.algorithms.diagnostics as diag
+from nipype.utils.filemanip import fname_presuffix
+
+def make_4d_nibabel(infiles,outdir=None):
+    shape = ni.load(infiles[0]).get_shape()
+    finalshape = tuple([x for x in shape]+[len(infiles)])
+    dat4d = np.empty(finalshape)
+    for val, f in enumerate(infiles):
+        tmpdat = ni.load(f).get_data()
+        tmpdat[np.isnan(tmpdat)] =  0
+        dat4d[:,:,:,val] = tmpdat
+    newimg = ni.Nifti1Image(dat4d, ni.load(infiles[0]).get_affine())
+    if outdir is None:
+        outf = fname_presuffix(infiles[0], prefix='data4d_')
+    else:
+        outf = fname_presuffix(infiles[0], prefix='data4d_',newpath = outdir)
+    newimg.to_filename(outf)
+    return outf
+
+        
+def make_4d(infiles):
+    merge = fsl.utils.Merge()
+    merge.inputs.in_files = infiles
+    merge.inputs.dimension = 't'
+    mrgout = merge.run()
+    return mrgout.outputs.merged_file
+    
+def outlier_vol_fromfile(mean, outliers, fname = None):
+    _, filename = os.path.split(mean)
+    vox = np.loadtxt(outliers).astype(int)
+    img = ni.load(mean)
+    newdat = np.zeros(img.get_shape()).flatten()
+    newdat[vox] = 100
+    newdat.shape = img.get_shape()
+    newimg = ni.Nifti1Image(newdat, img.get_affine())
+    if fname is None:
+        newfile = filename.replace('mean', 'outliers')
+    newfile = fname
+    newimg.to_filename(newfile)
+    print os.path.abspath(newfile)
+
+def gen_sig2noise_img(in4d, outdir):
+    startdir = os.getcwd()
+    os.chdir(outdir)
+    tsnr = misc.TSNR()
+    tsnr.inputs.in_file = in4d
+    tsnrout = tsnr.run()
+    ## there is no valid return code for tsnr 
+    #if tsnrout.runtime.returncode == 0:
+    #    return tsnrout.outputs.tsnr_file
+    #else:
+    #    print tsnrout.runtime.stderr
+    #    return None
+    os.chdir(startdir)
+    return tsnrout.outputs.tsnr_file
+
+def make_qa_dir(inroot, name='data_QA'):
+    qadir = os.path.join(inroot, name)
+    if os.path.isdir(qadir):
+        return qadir, True
+    else:
+        os.mkdir(qadir)
+        return qadir, False
+
+def run_artdetect(file4d, param_file):
+    startdir = os.getcwd()
+    pth, _ = os.path.split(file4d)
+    os.chdir(pth)
+    ad = rapidart.ArtifactDetect()
+    ad.inputs.realigned_files = file4d
+    ad.inputs.realignment_parameters = param_file
+    ad.inputs.parameter_source = 'SPM'
+    ad.inputs.norm_threshold = 1
+    ad.inputs.use_differences = [True, False]
+    ad.inputs.zintensity_threshold = 3
+    adout = ad.run()
+    os.chdir(startdir)
+    return adout
+
+def screen_data_dirnme(in4d, outdir):
+    """uses nipy diagnostic code to screen the data for
+    outlier values and saves results to three images
+    mean, std, pca, in same dir as original file(s)"""
+    img = nipy.load_image(in4d)
+    result = diag.screen(img)
+    # save mean, std, pca
+    pth, nme = os.path.split(in4d)
+    stripnme = nme.split('.')[0]
+    
+    pcafile = os.path.join(outdir,
+                           'QA-PCA_%s.nii.gz'%(nme))
+    meanfile = os.path.join(outdir,
+                            'QA-MEAN_%s.nii.gz'%(nme))
+    stdfile = os.path.join(outdir,
+                           'QA-STD_%s.nii.gz'%(nme))
+    nipy.save_image(result['mean'], meanfile)
+    nipy.save_image(result['std'], stdfile)
+    nipy.save_image(result['pca'], pcafile)
+    print 'saved: %s\n \t%s\n \t%s\n'%(pcafile, meanfile, stdfile)
+
+def save_qa_img_dirnme(in4d, outdir):
+    pth, nme = os.path.split(in4d)
+    img = nipy.load_image(in4d)
+    diag.plot_tsdiffs(diag.time_slice_diffs(img))
+    cleantime = time.asctime().replace(' ','-').replace(':', '_')
+    figfile = os.path.join(outdir, 'QA_%s_%s.png'%(nme, cleantime))
+    pylab.savefig(figfile)
+
+
+
+if __name__ == '__main__':
+
+    """
+    maybe add tool to calculate std for the unwarped_realigned run?)
+    """
+
+    startdir = os.getcwd()
+
+    mean = '/home/jagust/pib_bac/ica/data/templates/MNI152_T1_2mm_brain.nii.gz'
+    allsubids = glob('/home/jagust/pib_bac/ica/data/spm_*/B*')
+    allsubids.sort()
+    for sub in allsubids[:]:
+        if 'B11-216' in sub:
+            continue
+        _, subid = os.path.split(sub)
+        merged = '%s/%s_resid.nii.gz'%(sub, subid)
+        param_file = '%s/func/realign_unwarp/rp_%s0000.txt'%(sub, subid)
+
+        qadir, exists = make_qa_dir(sub)
+        os.chdir(qadir)
+        artout = run_artdetect(merged, param_file)
+        vox_outliers =  artout.outputs.outlier_files
+        tsnr_file = gen_sig2noise_img(merged)
+        outlier_vol_fromfile(mean, vox_outliers,
+                             fname='%s_outliers.nii.gz'%(subid))
+        statd = json.load(open(artout.outputs.statistic_files))
+        mot = statd[1]['motion_outliers']
+        intensity = statd[1]['intensity_outliers']
+        if mot > 0:
+            tmp = np.loadtxt(artout.outputs.outlier_files)[:mot]
+            tmp.tofile('bad_frames.txt', sep='\n')
+        np.array([mot,intensity]).tofile('motion_intensity_outliers',
+                                         sep = '\n')
+        
+
+        print subid
+        ## get slice time unwar realigned files
+        sufiles = glob('%s/func/slicetime/auB*')
+        sufiles.sort()
+        merged_su = make_4d(sufiles)
+        screen_data_dirnme(merged_su, qadir)
+        os.chdir(startdir)
+"""
+    ## detect bad scans
+    allstats = glob('stats.*')
+    for statf in allstats:
+        m = re.search('B[0-9]{2}-[0-9]{3}',statf)
+        subid = m.group()
+        tmpd = json.load(open(statf))
+        motion_outliers = tmpd[1]['motion_outliers']
+        intensity_out = tmpd[1]['intensity_outliers']
+        print subid, motion_outliers, intensity_out
+        if motion_outliers > 0:
+            artifacts = glob('art.*%s*'%(subid))[0]            
+            tmp = np.loadtxt(artifacts)
+            badframes = tmp[:motion_outliers]
+            print 'bad frames', badframes
+
+
+    
+            # compute normalized intensity values
+            gz = signal.detrend(g,axis=0)       # detrend the signal
+            if self.inputs.use_differences[1]:
+                gz = np.concatenate( (np.zeros((1,1)),np.diff(gz,n=1,axis=0)) , axis=0)
+            gz = (gz-np.mean(gz))/np.std(gz)    # normalize the detrended signal
+            iidx = find_indices(abs(gz)>self.inputs.zintensity_threshold)
+
+            outliers = np.unique(np.union1d(iidx,np.union1d(tidx,ridx)))
+            artifactfile,intensityfile,statsfile,normfile,plotfile = self._get_output_filenames(imgfile,cwd)
+"""
