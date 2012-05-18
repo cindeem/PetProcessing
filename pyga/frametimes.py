611d64afd2a1f2dd932726cd9b7728d58f364ce6
CindeeM <cindee@berkeley.edu>
2012-05-18 15:08:24 -0700

    NF : added logan, frametimes tool

---
+++ pyga/frametimes.py
@@ -0,0 +1,184 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+import sys, os
+import nibabel.ecat as ecat
+sys.path.insert(0, '/home/jagust/cindeem/src/pydicom-0.9.7')
+import dicom
+import numpy as np
+from datetime import datetime
+import csv
+from glob import glob
+
+def get_series_iter(infile):
+    """ based on structure of current dicoms
+    returns iterator for files to junm between
+    frames"""
+    plan = dicom.read_file(infile)
+    ns = plan.NumberOfSlices
+    nts = plan.NumberOfTimeSlices
+    ii = plan.ImageIndex
+    return np.arange(0,nts*ns, ns)
+
+
+def sort_series(infiles):
+    nfiles = len(infiles)
+    out = np.recarray((nfiles), dtype=[('x', int), ('file', 'S250')])
+    for val, f in enumerate(infiles):
+        plan = dicom.read_file(f)
+        ii = plan.ImageIndex
+        out.x[val] = ii
+        out.file[val] = f
+    sort_mask = out.argsort(axis=0)    
+    out = out[sort_mask]# sort to correct order
+    return out
+
+
+def frametime_from_dicoms(infiles):
+    """ given a dicom series,
+    find timing info for each frame
+    """
+    frametimes = []
+    files = []
+    sorted = sort_series(infiles)
+    fiter = get_series_iter(infiles[0])
+    for f in sorted.file[fiter]:
+        plan = dicom.read_file(f)
+        st = datetime.fromtimestamp(float(plan.StudyTime))
+        at = datetime.fromtimestamp(float(plan.AcquisitionTime))
+        dur = float(plan.ActualFrameDuration)
+        start = (at -st).microseconds * 1000
+        frt = datetime.fromtimestamp(float(plan.FrameReferenceTime))
+        end = start + dur
+        frametimes.append([frt.microsecond * 1000, at.microsecond * 1000, dur, st.microsecond * 1000])
+        files.append(f)
+    return frametimes, files
+        
+
+        
+def frametime_from_ecat(ecatf):
+    """given an ecat file , finds timing info for each frame
+    
+    Returns
+    -------
+    out : array
+          array holding framenumber, starttime, duration, endtime
+          in milliseconds
+    """
+    img = ecat.load(ecatf)
+    shdrs = img.get_subheaders()
+    mlist = img.get_mlist()
+    framenumbers = mlist.get_series_framenumbers()
+    out = np.zeros((len(framenumbers), 4))
+    if shdrs.subheaders[0]['frame_start_time']== 16:
+        adj = 16
+    else:
+        adj = 0
+    for i, fn in framenumbers.items():
+        startt = shdrs.subheaders[i]['frame_start_time']
+        dur = shdrs.subheaders[i]['frame_duration']
+        out[i,0] = fn 
+        out[i,1] = startt - adj
+        # fix adj, divide by 60000 to get minutes
+        out[i,2] = dur
+        out[i,3] = startt - adj + dur
+    return out
+
+
+def frametimes_from_ecats(filelist):
+    """
+    for each ecat file in filelist
+    gets the frame number, start, duration info
+    combines
+    sorts and retruns
+
+    Returns
+    -------
+    out : array
+          array holding framenumber, starttime, duration, endtime
+          in milliseconds
+          
+    """
+    if not hasattr(filelist, '__iter__'):
+        filelist = [filelist]
+    for f in filelist:
+        tmp = frametime_from_ecat(f)
+        try:
+            out = np.vstack((out, tmp))
+        except:
+            out = tmp
+    return out
+
+
+def frametimes_to_seconds(frametimes):
+    """ assumes a frametimes array
+    [frame, start, duration, stop]
+    converts to seconds"""
+    newframetimes = frametimes.copy()
+    newframetimes[:,1:] = frametimes[:,1:] / 1000.
+    return newframetimes
+
+def make_outfile(infile, name = 'frametimes'):
+    pth, _ = os.path.split(infile)
+    newname = name + datetime.now().strftime('-%Y-%m-%d_%H-%M') + '.csv'
+    newfile = os.path.join(pth, newname)
+    return newfile
+
+def write_frametimes(inarray, outfile):
+    csv_writer = csv.writer(open(outfile, 'w+'))
+    csv_writer.writerow(['frame', 'start', 'duration','stop'])
+    for row in inarray:
+        csv_writer.writerow(row)
+
+def read_frametimes(infile):
+    outarray = []
+    jnk = csv.reader(open(infile),delimiter=',' )
+    for row in jnk:
+        if not 'frame' in row[0]:
+            outarray.append(row)
+    return np.asarray(outarray, dtype=float)
+
+
+
+
+
+if __name__ == '__main__':
+    #run tests
+    infile = 'test/pibecat_frames34.v'
+    ft = frametime_from_ecat(infile)
+    np.testing.assert_equal(ft[0,0], 1)
+    np.testing.assert_equal(ft[0,1] , 0)
+    np.testing.assert_equal(ft[0,2] , 15000)
+    np.testing.assert_equal(ft[0,3] , 15000)
+
+    # test naming
+    outf = make_outfile(infile)
+    np.testing.assert_equal('test/frametimes' in outf, True)
+
+
+    # test writing
+    ft_sec = ft.copy()
+    ft_sec[:,1:] = ft_sec[:,1:] / 1000.
+    
+    write_frametimes(ft, outf)
+    outf_sec = make_outfile(infile, name = 'frametimes_sec')
+    write_frametimes(ft_sec, outf_sec)
+
+    # roundtrip
+    ft = read_frametimes(outf)
+    np.testing.assert_equal(ft[0,0], 1)
+    np.testing.assert_equal(ft[0,1] , 0)
+    np.testing.assert_equal(ft[0,2] , 15000)
+    np.testing.assert_equal(ft[0,3] , 15000)
+    eft = read_frametimes(outf)
+    os.unlink(outf)
+
+    ## dicom
+    inglob = '../../biograph_dicom/B12-219PIBFR1TO18/*'
+    alldicoms = glob(inglob)
+    ft, files = frametime_from_dicoms(alldicoms)
+
+    inglob = '../../biograph_dicom/B12-209FDG/*'
+    alldicoms = glob(inglob)
+    ft2, files2 = frametime_from_dicoms(alldicoms)
+
+    
---
+++ pyga/py_logan.py
@@ -0,0 +1,236 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+import sys, os
+from glob import glob
+import time
+import numpy as np
+import nibabel as ni
+import scipy.integrate
+#from scipy.weave import inline, converters
+import matplotlib.pyplot as plt
+import tempfile
+
+ 
+def get_ref(refroi, dat):
+    """given region of interest, extracts mean for each frame
+    in dat, returns vector of means across time
+    """
+    refdat = ni.load(refroi).get_data().squeeze()
+    if not refdat.shape == dat.shape[1:]:
+        raise IOError('%s has shape %s, not %s'%(refroi, refdat.shape, dat.shape[1:]))
+    means = np.zeros(dat.shape[0])
+    refdat = np.nan_to_num(refdat)
+    for val,slice in enumerate(dat):
+        ind = np.logical_and(slice > 0,
+                             refdat > 0)
+        means[val] = slice[ind].mean()
+    return means
+
+
+def midframes_from_file(infile, units='sec'):
+    """infile is a frametimes file each row has
+    [frame number, start dur, stop] in seconds
+
+    Returns
+    -------
+    midframes: vector of start-dur/2
+    starttimes: vector of durations
+    """
+    ft = np.loadtxt(infile, delimiter = ',',
+                    usecols = (1,2,3), skiprows = 1)
+    midframes = ft[:,0] + ft[:,1] / 2
+    return midframes, ft[:,1]
+
+def frametimes_from_file(infile):
+    """infile is a frametimes file each row has
+    [frame number, start dur, stop] in seconds
+
+    Returns
+    -------
+    ft: array [start, duration, stop] in seconds for each frame
+    """
+    ft = np.loadtxt(infile, delimiter = ',',
+                    usecols = (1,2,3), skiprows = 1)
+    return ft
+
+def is_iterable(input):
+    """checks if object can be iterated"""
+    if hasattr(input, '__iter__'):
+        return True
+    else:
+        return False
+
+def load_3d(infiles):
+    """given a list of 3d frames, load into 4D array
+    retun array of shape (frame, x, y, z) with nan removed"""
+    nfiles = len(infiles)
+    shape = ni.load(infiles[0]).get_shape()
+    newshape = tuple([len(frames)] + [x for x in shape])
+    outdata = np.zeros(newshape)
+    for val, f in enumerate(infiles):
+        outdata[val,:,:,:] = ni.load(f).get_data().squeeze()
+    return np.nan_to_num(outdata)
+
+def get_data_nibabel(infiles):
+    """ uses nibabel to open nifti file
+    and return a 4d array of data"""
+    if is_iterable(infiles):
+        # set of 3D frames
+        dat4d = load_3d(infiles)
+        return dat4d
+    else:
+        print '4d files not supported yet'
+        return None
+
+def mask_data(mask, dat4d):
+    """given a mask file and a 4d array
+    mask data with data in maskfile
+    return maksed_data and mask_bool"""
+    maskdat = ni.load(mask).get_data().squeeze()
+    if not maskdat.shape == dat4d.shape[1:]:
+        raise IOError('shape mismatch, %s: %s and dat4d: %s'%(mask,
+                                                              maskdat.shape,
+                                                              dat4d.shape))
+    out = [slice[maskdat>0] for slice in dat4d]
+    return np.asarray(out), maskdat > 0
+
+def get_ki_vd_lstsq(x,y):
+    """solves best fitting line using np.linalg.lstsq
+    """
+    x = np.vstack([x,np.ones(x.shape[0])]).T
+    results = np.linalg.lstsq(x,y)
+    ki, vd = results[0]
+    residues = results[1]
+    return ki,vd,residues
+
+def get_ki_vd_polyfit(x,y):
+    """ solves using np.polyfit
+    """
+    output = np.polyfit(x,y,1,full=True)
+    ki,vd = output[0]
+    resids = output[1]
+    return ki, vd, resids
+
+
+
+def integrate_reference(reference_data, delta_time):
+    """
+    returns a cumulative integration of reference_data with delta_time
+    uses np.cumtrapz"""
+    int_ref = np.zeros(reference_data.shape)
+    int_ref[1:] = scipy.integrate.cumtrapz(reference_data, delta_time)
+    return int_ref
+
+    
+def save_inputplot(ref, midframes, outdir):
+    """saves input TAC to png figure
+    """
+    fig = plt.figure()
+    fig.set_label('Reference TAC')
+    ax1 = fig.add_subplot(111)
+    ax1.plot(midframes,ref, 'ko-')
+    ax2 = ax1.twiny()
+    ax1.set_xlabel('midframe times (sec)')
+    ax1.set_ylabel('counts bq/ml')
+    ax2.set_xlabel('midframe times (min)')
+    xticks_minutes = (ax1.get_xticks() / 60.).round()
+    ax2.set_xticks(xticks_minutes)
+    ax1.grid()
+    basename = 'REF_TAC_%s'%(time.strftime('%Y-%m-%d-%H-%M'))
+    figname = os.path.join(outdir, '%s.png'%(basename))
+    plt.savefig(figname, format='png')  
+    return figname
+  
+def save_data2nii(data, reference_img, filename='generic_file',outdir='.'):
+    """saves data to nifti file given affine info in reference_img
+    and data array"""
+    img = ni.load(reference_img)
+    data = np.reshape(data, img.get_shape())
+    out_img = ni.Nifti1Image(data, img.get_affine())
+    outfile = os.path.join(outdir, '%s_%s'%(filename,time.strftime('%Y-%m-%d-%H-%M')))
+    out_img.to_filename(outfile)
+    return outfile
+    
+
+def repmat_1d(x, n):
+    """ make new matrix of n copies of x
+    resulting array has shape (n, x.shape)"""
+    newx = np.tile(x, n)
+    newx.shape = tuple([n] + [i for i in x.shape])
+    return newx
+
+def calc_xy(ref, masked_dat,midtimes, k2ref=.15):
+    """calculates the x and y terms used in Logan Graphical Analysis
+    y = integrated_data / data
+    x = integrated_reference / data + (1 / k2ref) * reference / data
+    """
+    big_durs = repmat_1d(midtimes,masked_dat.shape[1])
+    int_dat = scipy.integrate.cumtrapz(masked_dat,big_durs.T,axis=0)
+    big_ref = repmat_1d(ref,masked_dat.shape[1])
+    int_ref = scipy.integrate.cumtrapz(big_ref.T,big_durs.T, axis=0)
+    y = (int_dat)  / masked_dat[1:,:]# 33, nvox in mask
+    x = (int_ref / masked_dat[1:,:]) + ( 1 / k2ref) *(big_ref.T[1:,:] / masked_dat[1:,:]) 
+    return x,y
+
+def get_lstsq(x,y):
+    """solves best fitting line using np.linalg.lstsq
+    """
+    newx = np.ones((x.shape[0],2))
+    newx[:,0] = x
+    results = np.linalg.lstsq(newx,y)
+    ki, vd = results[0]
+    residues = results[1]
+    return ki,vd,residues
+
+def calc_ki(ref, data, timing_file, range=(35,90)):
+    """ calculates ki of data given reference, timing file,
+    and range of steady state data (in minutes)"""
+    
+    midtimes, durs = midframes_from_file(timing_file)
+    ft = frametimes_from_file(timing_file)
+    x,y  = calc_xy(ref, data, midtimes)
+    allki = np.zeros(data.shape[1])
+    resids = np.zeros(data.shape[1])
+    start_end = np.logical_and(ft[1:,0] / 60. >= range[0],ft[1:,2] / 60. <= range[1])
+    for val, (tmpx, tmpy) in enumerate(zip(x.T,y.T)):
+        ki,vd,residues = get_lstsq(tmpx[start_end],tmpy[start_end])
+        allki[val] = ki
+        resids[val] = residues
+    return allki, resids
+
+def results_to_array(results, mask, shape):
+    """ puts values in results back in fill data array
+    of size shape using values in boolean mask"""
+    dat = np.zeros(shape)
+    dat[mask] = results
+    return dat
+
+if __name__ == '__main__':
+
+    root = '/home/jagust/cindeem/CODE/GraphicalAnalysis/pyGA_refactor/test/pib2'
+    k2ref = 0.15
+    range = (35,90)
+    frames = glob(root + '/rB*PIB*.nii')
+    frames.sort()
+    refroifile = '%s/rgrey_cerebellum.nii'%root
+    mask = '%s/rbrainmask.nii'%root
+    timing_file = '%s/frametimes.csv'%root
+    midtimes, durs = midframes_from_file(timing_file)
+    data4d = get_data_nibabel(frames)
+    ref = get_ref(refroifile, data4d)
+    ref_fig = save_inputplot(ref, (midtimes + durs/2.), root)
+    masked_data, mask_roi = mask_data(mask, data4d)
+    allki, residuals = calc_ki(ref, masked_data, timing_file, range=range)
+    dvr = results_to_array(allki, mask_roi, data4d.shape[1:])
+    
+    save_data2nii(dvr, mask, filename='DVR', outdir=root)
+
+    
+
+
+            
+    
+            
+            
+        
+        
