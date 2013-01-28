90f99c81b2eebfa7baa9b834e1fe0611b0263293
cindeem <cindee@berkeley.edu>
2013-01-28 15:27:15 -0800

    NF: add nii2dcm script

---
+++ scripts/nii2dcm.py
@@ -0,0 +1,39 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+#!/usr/bin/env python
+
+import sys
+import argparse
+sys.path.insert(0,'/home/jagust/cindeem/CODE/petproc-refactor/preprocessing')
+import dicom_tools 
+
+if __name__ == '__main__':
+
+
+    # create the parser
+    parser = argparse.ArgumentParser(
+        description='Convert nifti mean image to dicoms')
+
+    # add the arguments
+    parser.add_argument(
+        'nifti', type=str, nargs=1,
+        help='nifti Filename')
+
+    parser.add_argument(
+        'dicomdir', type=str, nargs=1,
+        help='Directory containing original dicoms')
+    
+
+    parser.add_argument(
+        'outdir', type=str, nargs=1,
+        help='Directory to save new dicoms')
+    
+        
+    if len(sys.argv) ==1:
+        parser.print_help()
+    else:
+        args = parser.parse_args()
+        
+        dicom_tools.nifti_to_dicom(args.nifti[0],
+                                   args.dicomdir[0],
+                                   args.outdir[0])
