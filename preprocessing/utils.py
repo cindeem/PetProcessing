c3440b133175677443a2b0256063f216c9ff86de
cindeem <cindee@berkeley.edu>
2013-01-28 14:59:18 -0800

    RF: fix preprocessing

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -1,5 +1,10 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
+import os, sys
+import re
+import tempfile
+from nipype.interfaces.base import CommandLine
+from nipype.utils.filemanip import split_filename, fname_presuffix
 
 def tar_cmd(infile):
     """ given a ipped tar archive, untars"""
