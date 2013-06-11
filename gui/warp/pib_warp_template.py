393f943b3e0f7e0475c1ccbeabbd8f0993b1cbfd
cindeem <cindee@berkeley.edu>
2013-06-11 14:04:26 -0700

    NF: warp gui for pib

--- gui/warp/pib_warp_template.py
+++ gui/warp/pib_warp_template.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     app = wx.App()
 
     arda = '/home/jagust/arda/lblid'
-    root = bg.SimpleDirDialog(prompt='Choose FDG data dir',
+    root = bg.SimpleDirDialog(prompt='Choose PIB data dir',
                               indir = '/home/jagust')
     
     cleantime = asctime().replace(' ','-').replace(':', '-')
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     logging.config.dictConfig(log_settings)
 
     
-    tracer = 'FDG'
+    tracer = 'PIB'
     user = os.environ['USER']
     logging.info('###START %s :::'%(__file__))
     logging.info('###TRACER  %s  :::'%(tracer))
@@ -48,54 +48,54 @@ if __name__ == '__main__':
     template = str(template[0])
     _, tname, _ = pp.split_filename(template)
                  
+    # text dialog to get the mean20minute pib and dvr 
+    mean20_glob = bg.TextEntry(message='Glob for mean20minute pseudo anat file',
+                                 default = 'mean20min*')
+    logging.info(mean20_glob)
 
-    # text dialog to get normed FDG file and coreg directory
-    norm_fdg_glob = bg.TextEntry(message='Enter Ref-normed FDG glob',
-                                 default = 'rpons_tunormed_mean*')
-    logging.info(norm_fdg_glob)
-
+    dvrdir_glob = bg.TextEntry(message='Glob for dvr dirrectory',
+                               default = 'dvr_rgrey_cerebellum')
+    logging.info(dvrdir_glob)
 
 
     for sub in subs:
+        if '_v2' in sub:
+            logging.info('skipping visit2 %s'%(sub))
+            continue
         _, subid = os.path.split(sub)
         logging.info('%s'%subid)
         anatdir = os.path.join(sub, 'anatomy')
         if not os.path.isdir(anatdir):
             logging.error('%s doesnt exist,skipping'%(anatdir))
             continue
-        tracerdir = os.path.join(sub,'fdg')
-        if not os.path.isdir(tracerdir):
-            logging.error('%s doesnt exist,skipping'%(tracerdir))
+        dvrdir = os.path.join(sub,'pib',dvrdir_glob)
+        dvrdir = glob(dvrdir)
+        
+        if len(dvrdir) < 1:
+            logging.error('%s doesnt exist,skipping'%(dvrdir))
             continue
-        #get ponsnormed
-        globstr = os.path.join(tracerdir, norm_fdg_glob)
-        pnfdg = utils.find_single_file(globstr)
-        if pnfdg is None:
-            logging.error('%s not found. skipping'%globstr)
-            shutil.rmtree(warpdir)
-            continue
-        pnfdg = utils.unzip_file(pnfdg)
-        # get summed fdg
-        if 'mean' in norm_fdg_glob:
-            tmp_glob = 'mean_rB*.nii*'
-        else:
-            tmp_glob = 'sum_rB*.nii*'
-        globstr = os.path.join(tracerdir,  tmp_glob)
-        sumfdg = utils.find_single_file(globstr)
-        if sumfdg is None:
-            logging.error('%s not found. skipping'%globstr)
-            shutil.rmtree(warpdir)
-            continue
-        sumfdg = utils.unzip_file(sumfdg)        
+        dvrdir = dvrdir[0]
         # make/check warp dir
-        (warpdir,
-         exists) = utils.make_dir(tracerdir,
-                                  'warp_%s_%s'%(norm_fdg_glob.replace('*',''),
-                                                tname))
+        warpdir, exists = utils.make_dir(dvrdir, 'warp_%s'%tname)
         if exists:
             logging.warning('%s exists, remove to rerun'%(warpdir))
             continue
-
+        #get dvr, brainmask
+        globstr = os.path.join(dvrdir, 'DVR-%s*.nii*'%subid)
+        dvr = utils.find_single_file(globstr)
+        if dvr is None:
+            logging.error('%s not found. skipping'%globstr)
+            shutil.rmtree(warpdir)
+            continue
+        dvr = utils.unzip_file(dvr)
+        # get mean 20 minute pib
+        globstr = os.path.join(sub,'pib', 'realign_QA', mean20_glob)
+        mean20 = utils.find_single_file(globstr)
+        if mean20 is None:
+            logging.error('%s not found. skipping'%globstr)
+            shutil.rmtree(warpdir)
+            continue
+        mean20 = utils.unzip_file(mean20)        
         # brainmask
         globstr = os.path.join(anatdir, 'brainmask.nii*')
         brainmask = utils.find_single_file(globstr)
@@ -103,54 +103,35 @@ if __name__ == '__main__':
             logging.error('%s not found. skipping'%globstr)
             shutil.rmtree(warpdir)
             continue
-        brainmask = utils.unzip_file(brainmask)
-        ## aparc
-        globstr = os.path.join(anatdir, 'B*aparc_aseg.nii*')
-        aparc = utils.find_single_file(globstr)
-        if aparc is None:
-            logging.error('%s not found. skipping'%globstr)
-            shutil.rmtree(warpdir)
-            continue
-
         # copy to warp dir
-        csumfdg = utils.copy_file(sumfdg, warpdir)
-        cpnfdg = utils.copy_file(pnfdg, warpdir)
+        cmean20 = utils.copy_file(mean20, warpdir)
+        cdvr = utils.copy_file(dvr, warpdir)
         cbm = utils.copy_file(brainmask, warpdir)
-        caparc = utils.copy_file(aparc, warpdir)
-
         # Mask brainmask with aparc to remove skull
         globstr = os.path.join(anatdir, '%s*aparc_aseg.nii*'%subid)
         aparc = utils.find_single_file(globstr)
         sscbm = fsl_tools.fsl_mask(cbm, aparc,
-                            outname = 'aparcmasked_brainmask.nii.gz')
+                                    outname = 'aparcmasked_brainmask.nii.gz')
         sscbm = utils.unzip_file(sscbm)
-
-        # coreg pet to masked brainmask
+        # coreg pet to brainmask
         logging.info('Run coreg')
         # cast everything to string
         sscbm = str(sscbm)
-        csumfdg = str(csumfdg)
-        cpnfdg = str(cpnfdg)
-        corgout = spm_tools.simple_coregister(sscbm, csumfdg, other=[cpnfdg])
+        cmean20 = str(cmean20)
+        cdvr = str(cdvr)
+        corgout = spm_tools.simple_coregister(sscbm, cmean20, other=[cdvr])
         if not corgout.runtime.returncode == 0:
             logging.error('coreg pet to mri failed %s'%subid)
             shutil.rmtree(warpdir)
             continue
-        rcsumfdg = corgout.outputs.coregistered_source
-        rpnfdg = corgout.outputs.coregistered_files
-        # warp brainmask to template, apply to pnfdg
+        rcmean20 = corgout.outputs.coregistered_source
+        rdvr = corgout.outputs.coregistered_files
+        # warp brainmask to template, apply to dvr
         logging.info('Run warp')
-        wout = spm_tools.simple_warp(template, sscbm, other= [rpnfdg])
+        wout = spm_tools.simple_warp(template, sscbm, other=[rdvr])
         if not wout.runtime.returncode == 0:
             logging.error('warp to template failed %s'%subid)
             shutil.rmtree(warpdir)
             continue            
-        ## zip files
-        cmd = 'gzip %s/*.nii'%warpdir
-        out = utils.CommandLine(cmd).run()
-        if out.runtime.returncode == 0:
-            logging.info('zipped files in %s'%warpdir)
-        else:
-            logging.warn('zipping failed for files in %s'%warpdir)
         logging.info('Finished warping %s'%subid)
-               
+        
